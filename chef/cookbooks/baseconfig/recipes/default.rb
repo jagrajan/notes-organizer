74316731bb27e581c8f9e7f0e35adbd30e63f80b
Jagrajan Bhullar <jag@jagrajan.com>
2018-08-28 16:13:09 -0700

    start vagrant code

---
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -0,0 +1,20 @@
+# Make sure the Apt package lists are up to date, so we're downloading versions that exist.
+execute 'apt_update' do
+  command 'apt-get update'
+end
+execute 'apt_upgrade' do
+  command 'apt-get upgrade -y'
+end
+
+# Base configuration recipe in Chef.
+package "wget"
+package "ntp"
+
+cookbook_file "ntp.conf" do
+  path "/etc/ntp.conf"
+end
+execute 'ntp_restart' do
+  command 'service ntp restart'
+end
+
+package "mysql-server"
